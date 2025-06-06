176c348d521ef299a5c042787e271d017746cb22
Mati47-A <mat.acevedoo@duocuc.cl>
2025-06-06 11:04:00 -0400

    version 3
    contador de numeros pares

--- creacion_numeros_aleatorios.py
+++ creacion_numeros_aleatorios.py
@@ -7,4 +7,11 @@ for i in range(100): #ciclo para repetir 100 veces
 numero=random.randint(1,100) #rango para el numero random
 
 numeros.sort()#ordenar de menor a mayor
-print(numeros)
+print(numeros)#mostrar lista
+
+#contador de numeros pares
+contador=0
+for i in range (len(numeros)):
+    if numeros[i]%2==0: #%=el resto de una division
+        contador+=1
+print(f"cantidad de numeros pares : {contador}")
